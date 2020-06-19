4b687815afec80e64b902ccad686e6fb18e9c443
Mike Rourke <mike.w.rourke@gmail.com>
2020-06-19 14:50:02 -0700

    Update cook-the-books to use json-server instead of jsonstore.io

--- chapter-07-cook-the-books/README.md
+++ chapter-07-cook-the-books/README.md
@@ -1,5 +1,12 @@
 # Chapter 7: Creating an Application from Scratch
 
+## Important Note!
+Unfortunately, `jsonstore.io` is no longer available. Since we only need to run
+this locally and depending on a cloud-based service isn't the best course of
+action, I've modified this code to use the `json-server` library. You no longer
+need to change the `API_URL` in `/store/api.js`. Just run `npm start` and you're
+ready to go!
+
 ## Overview
 The code within this folder represents the complete application, 'Cook the Books', built in Chapter 7.
 
--- chapter-07-cook-the-books/src/store/api.js
+++ chapter-07-cook-the-books/src/store/api.js
@@ -1,9 +1,4 @@
-// Replace [JSONSTORE.IO ENDPOINT]' with your jsonstore.io endpoint below (no ending slash):
-// Make sure you remove the "[" and "]" from the string.
-const API_URL = '[JSONSTORE.IO ENDPOINT]';
-
-// Example:
-// const API_URL = 'https://www.jsonstore.io/ba365164c3806df04e7e6f22321adfe8897b6f17eaa4f9ce6e298f1d123';
+const API_URL = 'http://localhost:4001';
 
 /**
  * Wrapper for performing API calls. We don't want to call response.json()
@@ -22,44 +17,21 @@ const performApiFetch = (endpoint = '', init = {}) =>
   }).then(response => response.json());
 
 export const apiFetchTransactions = () =>
-  performApiFetch('/transactions').then(({ result }) =>
-    /*
-     * The response object looks like this:
-     * {
-     *   "result": {
-     *     "1": {
-     *       "category": "Sales Revenue",
-     *       ...
-     *     },
-     *     "2": {
-     *       "category": "Hotels",
-     *       ...
-     *     },
-     *     ...
-     *   }
-     * }
-     * We need the "1" and "2" values for deleting or editing existing
-     * records, so we store that in the transaction record as "apiId".
-     */
-    Object.keys(result).map(apiId => ({
-      ...result[apiId],
-      apiId
-    }))
-  );
+  performApiFetch('/transactions');
 
 export const apiEditTransaction = transaction =>
-  performApiFetch(`/transactions/${transaction.apiId}`, {
-    method: 'POST',
+  performApiFetch(`/transactions/${transaction.id}`, {
+    method: 'PUT',
     body: JSON.stringify(transaction)
   });
 
 export const apiRemoveTransaction = transaction =>
-  performApiFetch(`/transactions/${transaction.apiId}`, {
+  performApiFetch(`/transactions/${transaction.id}`, {
     method: 'DELETE'
   });
 
 export const apiAddTransaction = transaction =>
-  performApiFetch(`/transactions/${transaction.apiId}`, {
+  performApiFetch("/transactions", {
     method: 'POST',
     body: JSON.stringify(transaction)
   });
